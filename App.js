0f0ccb8ddf118c6aba649a366b406e6ad99903a9
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2022-10-09 23:25:30 -0300

    Atualizado

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet, Animated} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <KeyboardAvoidingView style={styles.background}>
+    {/* para nao bugar o teclado que fica em cima do teclado no IOS*/}
+      <View style={styles.viewHeaderLogo}>
+        <Text style={styles.headerText}>Hello Again!</Text>
+        <Text style={styles.headerSmallText}>Are you ready to workout?</Text>
+      </View>
+      <View style={styles.containerMainApp}>
+        <Text style={styles.containerTextSign}>SIGN IN</Text>
+        <TextInput
+          style={styles.inputContainer}
+          placeholder='E-mail'
+          autoCorrect={false}
+          onChangeText={() => {}}/*nao vai salvar em canto nenhum, botar funcao anonima*/
+        />
+        <TextInput
+          style={styles.inputContainer}
+          placeholder='Password'
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>L O G I N</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.haventAccountButton}>
+          <Text style={styles.textAccount}>Don't have an account? <Text style={styles.accountGreen}>SIGN UP</Text></Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.viewHeaderLogo}>
+        <Image 
+          source={require('./assets/logo.png')}
+        />
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#101010'
   },
+  viewHeaderLogo:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerMainApp:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 60,
+  },
+  inputContainer:{
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 20,
+    color: '#222',
+    fontSize: 12,
+    borderRadius: 5,
+    padding: 20,
+  },
+  loginButton:{
+    backgroundColor: '#24FF00',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  loginButtonText:{
+    color: '#101010',
+    fontSize: 15,
+    fontWeight: '600',
+  },
+  haventAccountButton:{
+    marginTop: 8,
+  },
+  textAccount:{
+    color: '#fff'
+  },
+  accountGreen:{
+    color: '#24FF00',
+    fontWeight: '900',
+  },
+  headerText:{
+    fontSize: 50,
+    fontWeight: '800',
+    color: '#24FF00',
+  },
+  headerSmallText:{
+    color: '#fff',
+    fontSize: 20,
+  },
+  containerTextSign:{
+    color: '#24FF00',
+    fontSize: 30,
+    fontWeight: '800',
+    marginBottom: 30,
+  },
+
 });
